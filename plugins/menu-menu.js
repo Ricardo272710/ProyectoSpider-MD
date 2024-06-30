d27d1958efd2f073f9fa63de33848daa9f59c466
Ricardo272710 <168157929+Ricardo272710@users.noreply.github.com>
2024-06-30 18:07:09 -0500

    Update menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -536,7 +536,7 @@ console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗
 console.log(e)}}
 
 //handler.command = /^(menu|menú|memu|memú|help|info|comandos|2help|menu1.2|ayuda|commands|commandos|menucompleto|allmenu|allm|m|\?)$/i
-handler.command = /^(menucompleto|menu|\?)$/i
+handler.command = /^(menucompleto|menu|menú\?)$/i
 export default handler
 
 const more = String.fromCharCode(8206)
